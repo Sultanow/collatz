dd308c7b3b236f4266b5ced79683d57646bf138a
rostalski <rostalski@lbgi.fr>
2021-06-28 16:39:22 +0200

    Added logs and new run

--- 01 Graph Theory/C/test.sh
+++ 01 Graph Theory/C/test.sh
@@ -10,7 +10,14 @@ then
 fi
 
 ./threadproc $1 > ./results/result.csv &
-cat /proc/$! 
+
+pid=$!
+
+while kill -0 $pid > /dev/null 2>&1
+do
+	ps aux | grep $! | grep -v grep | awk 'BEGIN { sum=0 } {sum=sum+$6; } END {printf("Taille RAM utilisée: %s Mo\n",sum / 1024)}' 
+	sleep 1
+done
 
 end=$(date +%s)
 
